127e4c1f2b89d9ae61b0c61e1c6cce5a1409c4b8
Roman Chukh <roman.chukh@gmail.com>
2014-10-12 16:17:46 +0300

    refactor: use logstash lwrp and exclude all other cookbooks from recipe
    Closes #7

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,15 @@
-name             'chamber-logmanager'
+name             'chamber-logstash'
 maintainer       'Roman Chukh'
 maintainer_email 'roman.chukh@gmail.com'
 license          'Apache 2.0'
-description      'Set of recipes for installing misc logging tools'
+description      'Installs and configures LogStash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.3'
+version          '0.1.0'
 
 supports 'centos'
 
-depends 'chef-sugar', '~> 2.3'
-
 depends 'java', '~> 1.28.0'
-depends 'elasticsearch', '~> 0.3.10'
 depends 'logstash', '~> 0.9.2'
 
-depends 'chamber-kibana', '~> 0.0.2'
+suggests 'elasticsearch', '~> 0.3.10'
+suggests 'chamber-kibana', '~> 0.0.2'
